443fd0d369f951d610385ef49298d0989f95f2cf
Valerii <>
2019-04-28 22:47:45 +0300

    initial commit

---
+++ MyCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCV
+//
+//  Created by Valerii Petrychenko on 4/27/19.
+//  Copyright © 2019 Valerii Petrychenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
